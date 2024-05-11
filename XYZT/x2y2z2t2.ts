d599da6842fa47775900fcaddba028d65e0a7c58
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-05-11 01:26:45 -0700

    Update x2y2z2t2.ts

--- XYZT/x2y2z2t2.ts
+++ XYZT/x2y2z2t2.ts
@@ -13,19 +13,16 @@ export function x2(){
     for (let i = 0; i <= 4; i++)
         for (let j = 0; j <= 4; j++)
             for (let k = 0; k <= 4; k++)
-console.log(i, j, k)
 }
 export function y2(){
     for (let l = 0; l <= 0 && l <= 4; ++l || l--)
         for (let m = 0; m <= 0 && m <= 4; ++m || m--)
             for (let n = 0; n <= 0 && n <= 4; ++n || n--)
-console.log(l, m, n)
 }
 export function z2(){
     for (let o = 0; o >= -4; o--)
         for (let p = 0; p >= -4; p--)
             for (let q = 0; q >= -4; q--)
-console.log(o, p, q)
 }
 export function t2(){
     do `x2*y2`
@@ -42,10 +39,10 @@ export function t2(){
     })
 }
 export function InventorySlot(){
-    'x4';['q, p, o']
-    'y4';['n, m, l']
-    'z4';['k, j, i']
-    't4';['x4-y4-z4']
+    'x4'=['q, p, o']
+    'y4'=['n, m, l']
+    'z4'=['k, j, i']
+    't4'=['x4-y4-z4']
 }
 console.log(InventorySlot)
 
