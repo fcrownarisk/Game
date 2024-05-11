a145c64c4834be5a16c1ffa3325fb2b6e81ec9ec
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-05-11 01:43:40 -0700

    Update photo.ts

--- particle/photo.ts
+++ particle/photo.ts
@@ -1,12 +1,9 @@
-
-
- import '../state/coherentState' 
- import '../particle/photo'
- import '../particle/fermion'
- enum spin{
+import '../state/coherentState' 
+import '../particle/photo'
+import '../particle/fermion'
+enum spin{
      spin0 = 0
- }
- enum mass{
+}
+enum mass{
     mass0 = 0
- }
- console.log(spin,mass,'spin0','mass0')
+}
