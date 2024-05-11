7a320adc25c9aafcb7cbaea92bf5194d04355daf
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-05-11 01:49:41 -0700

    Update generalrelativity.ts

--- equation/generalrelativity.ts
+++ equation/generalrelativity.ts
@@ -1,19 +1,18 @@
-
-import '../Vector'
-import '../'
-import '../XYZT'
 void function tensor(){
-     void '19tensor'
-     void '91tensor'
-     void 'intensor*outtensor'
-     void 'inouttensor/outintensor'
+     void '19tensor + 91tensor'
+     void 'Nulltensor = Ntensor + NNtensor + NNNtensor'
+     void 'intensor * outtensor'
+     void 'inouttensor / outintensor'
+     void 'antiinouttensor % antioutintensor'
 }
+
 void function Vector(){
-    void  'Vector2D+Vector11D'
-    void  'Vector3D*Vector4D*Vector5'
-    void  'Vector7D/Vector8D'
-    void  'Vector9D%Vector10D%Vector11D'
+    void  'Vector2D' + 'Vector11D'
+    void  'Vector3D' * 'Vector4D' * 'Vector5D'
+    void  'Vector6D' / 'Vector7D'
+    void  'Vector8D' % 'Vector9D' % 'Vector10D'
 }
+
 void function XYZ() {
     void 'BigXtensor' + 'BigYtensor' + 'BigZtensor'
     void 'midxtensor' + 'midytensor' + 'midztensor'
