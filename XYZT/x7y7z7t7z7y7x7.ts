100e7531e8991fd2ae215de976dfc2433b71f2e4
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-05-12 02:40:00 -0700

    Update x7y7z7t7z7y7x7.ts

--- XYZT/x7y7z7t7z7y7x7.ts
+++ XYZT/x7y7z7t7z7y7x7.ts
@@ -6,12 +6,12 @@ import '../XYZT/t5z5y5x5'
 import '../XYZT/t6z6y6x6'
 import '../InventorySlots/N1InventorySlots'
 export function x7y7zt7z7y7x7(){
-    'x1y1z1t1;VideoDecoder'
-    'x2y2z2t2;VideoEncoder'
-    'x3y3z3t3;VideoFrame'
-    't4z4y4x4;VideoColorSpace'
-    't5z5y5x5;VideoPlaybackQuality'
-    't6z6y6x6;EncodedVideoChunk+RTCEncodedVideoFrame'
+    'x1y1z1t1=VideoDecoder'
+    'x2y2z2t2=VideoEncoder'
+    'x3y3z3t3=VideoFrame'
+    't4z4y4x4=VideoColorSpace'
+    't5z5y5x5=VideoPlaybackQuality'
+    't6z6y6x6=EncodedVideoChunk+RTCEncodedVideoFrame'
 }
 console.log(x7y7zt7z7y7x7)
 export function N1InventorySlot(){
