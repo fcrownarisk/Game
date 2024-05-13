c916f04936bb69761cd347fe8b13fc19c73c07ed
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-05-13 02:29:17 -0700

    Update x1y1z1t1.ts

--- XYZT/x1y1z1t1.ts
+++ XYZT/x1y1z1t1.ts
@@ -13,19 +13,16 @@ export function x1(){
     for (let i = 0; i <= 2; i++)
         for (let j = 0; i <= 2; j++)
             for (let k = 0; k <= 2; k++)
-console.log(i, j, k)
 }
 export function y1(){
     for(let l = 0;-2<=l && l<=3; l++||l--)
        for(let m = 0;-1<=m && m<=3 ;m++||m--)
           for(let n = 0;-2<=n && n<=3; n++||n--)
-console.log(l,m,n)
 }
 export function z1(){
     for(let o=0; o<=3; o++)
         for(let p=0; p<=3; p++)
             for(let q=0;q<=3; q--)
-console.log(o,p,q)
 }
 export function t1(){
         do `x1*y1`
