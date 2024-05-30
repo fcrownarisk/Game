ed37f13d22fabc5622cce787bf4a1d2b079f34c8
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-05-30 13:04:50 +0800

    Update opqstringtonumber.ts

--- midletter/opqrst/opqstringtonumber.ts
+++ midletter/opqrst/opqstringtonumber.ts
@@ -1,7 +1,6 @@
-import '../opqrst/opq'
 void function opq(o:string,p,String,Number,q:number) {
     void "o:parseFloat"
     void "p:parseIn"
-    void "q:Storage"
+    void "q:parse"
     return [o, p, q]
 }
